56434e6b1eb5e0f4e4ff36f5178f50f728a9f909
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 16:09:59 -0500

    add url for create and delete elephant

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
   path('elephants/', views.elephants_index, name='elephants_index'),
   path('elephants/<int:elephant_id>/', views.elephants_detail, name='elephants_detail'),
   path('elephants/create/', views.ElephantCreate.as_view(), name='elephants_create'),
-
+  path('elephants/<int:pk>/update/', views.ElephantUpdate.as_view(), name='elephants_update'),
+  path('elephants/<int:pk>/delete/', views.ElephantDelete.as_view(), name='cats_delete'),
 ]
