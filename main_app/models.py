118d352f219b31d509bcac9d51536c51afcdd15a
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 16:53:44 -0500

    add feeding class to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 class Elephant(models.Model):
   name = models.CharField(max_length=100)
@@ -11,4 +17,11 @@ class Elephant(models.Model):
 
   def get_absolute_url(self):
       return reverse("elephants_detail", kwargs={"elephant_id": self.id})
-  
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+  elephant = models.ForeignKey(Elephant, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
