a49071ddb7229e9e79ecdd549d24feb323df2621
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 15:44:12 -0500

    add route and update nav for create cat functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('elephants/', views.elephants_index, name='elephants_index'),
-  path('elephants/<int:elephant_id>/', views.elephants_detail, name='elephants_detail')
+  path('elephants/<int:elephant_id>/', views.elephants_detail, name='elephants_detail'),
+  path('elephants/create/', views.ElephantCreate.as_view(), name='elephants_create'),
 
 ]
