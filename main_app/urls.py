2d5c637c145190efd6c4e7b5e70a58cb8f126cff
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 14:45:32 -0500

    add index url and view with css

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('elephants/', views.elephants_index, name='elephants_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+class Elephant:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+elephants = [
+  Elephant('Lolo', 'kinda rude'),
+  Elephant('Achi', 'Looks majestic'),
+  Elephant('Fancy', 'happy')
+]
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def elephants_index(request):
+  return render(request, 'elephants/index.html', { 'elephants': elephants })
