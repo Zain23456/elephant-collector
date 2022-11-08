f0a73bbae78606c4eed0a84c4f9515b71371c135
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 19:28:25 -0500

    register toy in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Elephant, Feeding
+from .models import Elephant, Feeding, Toy
 # Register your models here.
 admin.site.register(Elephant)
 admin.site.register(Feeding)
+admin.site.register(Toy)
