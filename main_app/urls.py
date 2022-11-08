353022a794cb820180869efb557a1ab9aea66214
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-07 19:30:39 -0500

    create url for create toy

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path('elephants/create/', views.ElephantCreate.as_view(), name='elephants_create'),
   path('elephants/<int:pk>/update/', views.ElephantUpdate.as_view(), name='elephants_update'),
   path('elephants/<int:pk>/delete/', views.ElephantDelete.as_view(), name='elephants_delete'),
-  path('elephants/<int:elephant_id>/add_feeding/', views.add_feeding, name='add_feeding')
+  path('elephants/<int:elephant_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
 ]
