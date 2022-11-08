cca4e97b7557048ceb296ccbc8cf517168d42b75
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-08 14:47:16 -0500

    add cute elephant image

--- main_app/views.py
+++ main_app/views.py
@@ -49,7 +49,7 @@ def elephants_index(request):
 
 def elephants_detail(request, elephant_id):
   elephant = Elephant.objects.get(id=elephant_id)
-  toys_elephant_doesnt_have = Toy.objects.exclude(id_in = elephant.toys.all().values_list('id'))
+  toys_elephant_doesnt_have = Toy.objects.exclude(id__in = elephant.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'elephants/detail.html', { 'elephant': elephant, 'feeding_form': feeding_form, 'toys': toys_elephant_doesnt_have })
 
