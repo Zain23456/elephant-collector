41c3ce85b8ebb8389693703e7672e6e6ad8e70d2
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-08 09:37:36 -0500

    add url to add toy to elephant from detail view

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
   path('elephants/<int:pk>/update/', views.ElephantUpdate.as_view(), name='elephants_update'),
   path('elephants/<int:pk>/delete/', views.ElephantDelete.as_view(), name='elephants_delete'),
   path('elephants/<int:elephant_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('elephants/<int:elephant_id>/assoc_toy/<int:toy_id>', views.assoc_toy, name='assoc_toy'),
   path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
   path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
   path('toys/', views.ToyList.as_view(), name='toys_index'),
