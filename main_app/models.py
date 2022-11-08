d192445bbed604d45417f5a346bc662e85e50dfe
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-08 09:10:18 -0500

    add manytomany field to elephant model

--- main_app/models.py
+++ main_app/models.py
@@ -9,9 +9,20 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Elephant(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
@@ -31,13 +42,3 @@ class Feeding(models.Model):
     return f"{self.get_meal_display()} on {self.date}"
   class Meta:
     ordering = ['-date']
-
-class Toy(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
-
-  def __str__(self):
-    return self.name
-
-  def get_absolute_url(self):
-    return reverse('toys_detail', kwargs={'pk': self.id})
