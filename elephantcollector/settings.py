e46a240cd615e827a9c8f77e5eb089828bdbcea7
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-09 12:04:48 -0500

    add logout redirect to home page

--- elephantcollector/settings.py
+++ elephantcollector/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = 'cats_index'
+
+LOGIN_REDIRECT_URL = 'elephants_index'
+
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
