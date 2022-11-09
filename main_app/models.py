a2e6a074d4170d5ef736790ca88cfa7a66cfe723
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-09 18:05:45 -0500

    add photo to model

--- main_app/models.py
+++ main_app/models.py
@@ -44,3 +44,10 @@ class Feeding(models.Model):
     return f"{self.get_meal_display()} on {self.date}"
   class Meta:
     ordering = ['-date']
+
+class Photo(models.Model):
+  url = models.CharField(max_length=250)
+  elephant = models.OneToOneField(Elephant, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for cat_id: {self.elephant_id} @{self.url}"
