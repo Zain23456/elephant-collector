8e19933bf8a12a7cae24c1ef7e6bc37486cb2ed0
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-09 12:13:38 -0500

    add signup function for view

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Elephant, Toy
 from .forms import FeedingForm
 
@@ -69,3 +71,17 @@ def add_feeding(request, elephant_id):
 def assoc_toy(request, elephant_id, toy_id):
   Elephant.objects.get(id=elephant_id).toys.add(toy_id)
   return redirect('elephants_detail', elephant_id=elephant_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('elephants_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
