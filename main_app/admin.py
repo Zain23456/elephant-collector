9eba996ff6041b2ae7e582c2aafc14da0f0f6f79
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-11-09 19:59:12 -0500

    add photo to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Elephant, Feeding, Toy
-# Register your models here.
+from .models import Elephant, Feeding, Toy, Photo
+
 admin.site.register(Elephant)
 admin.site.register(Feeding)
 admin.site.register(Toy)
+admin.site.register(Photo)
